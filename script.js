a288439bcb7c7ef6bc79a43f7ae5f6eecbc58b09
mz-suzin <mzsuzin@gmail.com>
2023-03-03 18:36:39 -0300

    final commit, site done

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ const loader = document.getElementById('loader');
 
 // Global Variables
 let photosArray = [];
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 
 // Unsplash API
 const apiKey = 'QLnuq11UDpTQswOGYbG8-GGBiiVEWrB9GIX6Y03mW_0';
-const numberOfImages = 4;
+const numberOfImages = 6;
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${numberOfImages}&orientation=landscape`;
 
 // Helper Function to set attributes on DOM elements
@@ -17,9 +20,18 @@ function setAttributes(element, attributes) {
     }
 }
 
+// Check if all images were loaded on the screen
+function imageLoaded() {
+    if (imagesLoaded++ === totalImages - 1) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
+
 // Create elements for links & photos, and then add to DOM
 function displayPhotos() {
-    photosArray.forEach((photo, index) => {
+    totalImages = photosArray.length;
+    photosArray.forEach((photo) => {
         // create <a> to link to Unsplash
         const item = document.createElement('a');
         setAttributes(item, {
@@ -34,7 +46,9 @@ function displayPhotos() {
             alt: photo.alt_description,
             title: photo.alt_description
         });
-        
+
+        img.addEventListener('load', imageLoaded());
+
         // put <img> inside <a>, then put both inside imageContainer element
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -46,12 +60,22 @@ async function getPhotos() {
     try {
         const response = await fetch(apiURL);
         photosArray = await response.json();
-        console.log(photosArray);
         displayPhotos();
     } catch (error) {
         console.log('Ooops, something wrong is not right', error);
     }
 }
+
+// Check if scrolling is near bottom of page, if yes, load more photos
+window.addEventListener('scroll', () => {
+    // window.innerHeight is constant, since it's the size of the showing screen
+    // document.body.offsetHeight is also constant, only updating when something new is added to the page
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        imagesLoaded = 0;
+        getPhotos();
+    }
+})
 
 // On Load
 getPhotos();
